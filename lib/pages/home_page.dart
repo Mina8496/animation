ffa8668511ddad30bda2a931aabd4690e60ccbd0
Mina Kamil <mena.m4077@gmail.com>
2026-06-19 02:14:51 +0300

    chore: learn Transform

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -7,14 +7,9 @@ class HomePage extends StatefulWidget {
   State<HomePage> createState() => _HomePageState();
 }
 
+double scale = 0;
+
 class _HomePageState extends State<HomePage> {
-  double height = 150;
-
-  double width = 150;
-  Color color = Colors.blue;
-  double radius = 0;
-  double padding = 0;
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -24,28 +19,16 @@ class _HomePageState extends State<HomePage> {
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
           Center(
-            child: AnimatedContainer(
-              padding: EdgeInsets.all(padding),
-              duration: Duration(seconds: 2),
-              width: width,
-              height: height,
-
-              decoration: BoxDecoration(
-                color: color,
-                borderRadius: BorderRadius.circular(radius),
-              ),
-              child: Text("Route", style: TextStyle(fontSize: 25)),
+            child: Transform.scale(
+              scale: scale,
+              child: Container(width: 150, height: 150, color: Colors.blue),
             ),
           ),
           SizedBox(height: 15),
           ElevatedButton(
             onPressed: () {
               setState(() {
-                height = 250;
-                width = 250;
-                radius = 25;
-                color = Colors.brown;
-                padding = 50;
+                scale++;
               });
             },
             child: Text("Animate"),
