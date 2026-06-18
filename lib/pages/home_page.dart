de4f75f3696983921723275a89c5790ae386059d
Mina Kamil <mena.m4077@gmail.com>
2026-06-19 01:55:15 +0300

    learn : AnimatedContainer

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -11,6 +11,9 @@ class _HomePageState extends State<HomePage> {
   double height = 150;
 
   double width = 150;
+  Color color = Colors.blue;
+  double radius = 0;
+  double padding = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -21,12 +24,29 @@ class _HomePageState extends State<HomePage> {
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
           Center(
-            child: Container(width: width, height: height, color: Colors.blue),
+            child: AnimatedContainer(
+              padding: EdgeInsets.all(padding),
+              duration: Duration(seconds: 2),
+              width: width,
+              height: height,
+
+              decoration: BoxDecoration(
+                color: color,
+                borderRadius: BorderRadius.circular(radius),
+              ),
+              child: Text("Route", style: TextStyle(fontSize: 25)),
+            ),
           ),
+          SizedBox(height: 15),
           ElevatedButton(
             onPressed: () {
-              height = 250;
-              width = 250;
+              setState(() {
+                height = 250;
+                width = 250;
+                radius = 25;
+                color = Colors.brown;
+                padding = 50;
+              });
             },
             child: Text("Animate"),
           ),
